dd478c794d47ce9e3aed143ef4ab3bfb33e6fdfe
Paul Gross <pgross@gmail.com>
2014-11-23 15:49:15 -0600

    Run input code through elixir with docker.

--- web/controllers/problem_controller.ex
+++ web/controllers/problem_controller.ex
@@ -7,7 +7,13 @@ defmodule ElixirProblems.ProblemController do
     render conn, "index.html"
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"id" => id_string}) do
+    {id, _} = Integer.parse(id_string)
     render conn, "show.html", id: id
+  end
+
+  def update(conn, %{"id" => id, "code" => code}) do
+    {results, exit_code} = System.cmd("docker", ["run", "trenpixster/elixir", "elixir", "-e", code])
+    render conn, "results.html", id: id, code: code, results: results
   end
 end
