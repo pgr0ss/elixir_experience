98ee29aff8282b53ca24e20b0c4ca2d0c8e40258
Paul Gross <pgross@gmail.com>
2014-11-23 21:48:51 -0600

    Test multiline code blocks. Capture docker stderr as well as stdout.

--- lib/elixir_problems/docker.ex
+++ lib/elixir_problems/docker.ex
@@ -1,6 +1,6 @@
 defmodule ElixirProblems.Docker do
   def run(code) do
-    {output, exit_code} = System.cmd("docker", ["run", "trenpixster/elixir", "elixir", "-e", code])
+    {output, exit_code} = System.cmd("docker", ["run", "trenpixster/elixir", "elixir", "-e", code], stderr_to_stdout: true)
     {String.strip(output), exit_code}
   end
 end
--- test/elixir_problems/docker_test.exs
+++ test/elixir_problems/docker_test.exs
@@ -6,4 +6,22 @@ defmodule ElixirProblems.DockerTest do
     assert exit_code == 0
     assert output == "42"
   end
+
+  test "run a bad command" do
+    {output, exit_code} = ElixirProblems.Docker.run("garbage")
+    assert exit_code == 1
+    assert String.contains?(output, "undefined function garbage/0")
+  end
+
+  test "run a multiline elixir command" do
+    code = """
+      x = "hello"
+      y = "world"
+      z = x <> " " <> y
+      IO.puts(z)
+    """
+    {output, exit_code} = ElixirProblems.Docker.run(code)
+    assert exit_code == 0
+    assert output == "hello world"
+  end
 end
