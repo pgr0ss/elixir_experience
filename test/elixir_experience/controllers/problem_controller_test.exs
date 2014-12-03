5f53d6a096b29141fa71263ce126e0cbd3606c6a
Paul Gross <pgross@gmail.com>
2014-12-02 20:24:29 -0800

    Show the correct problem description on the results page.

---
+++ test/elixir_experience/controllers/problem_controller_test.exs
@@ -0,0 +1,20 @@
+defmodule ElixirExperience.ProblemControllerTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  test "show" do
+    conn = conn(:get, "/problems/1") |> ElixirExperience.Router.call([])
+
+    assert conn.status == 200
+
+    assert String.contains?(conn.resp_body, "prints the number 42") == true
+  end
+
+  test "update" do
+    conn = conn(:put, "/problems/1", %{"code" => "IO.puts 10"}) |> ElixirExperience.Router.call([])
+
+    assert conn.status == 200
+
+    assert String.contains?(conn.resp_body, "<pre>10</pre>") == true
+  end
+end
--- web/controllers/problem_controller.ex
+++ web/controllers/problem_controller.ex
@@ -16,6 +16,6 @@ defmodule ElixirExperience.ProblemController do
     {id, _} = Integer.parse(id_string)
     problem = ElixirExperience.ProblemList.get_problem(id)
     {output, exit_code} = ElixirExperience.Docker.run(code)
-    render conn, "results.html", id: id, code: code, output: output, correct: output == problem.answer
+    render conn, "results.html", id: id, problem: problem, code: code, output: output, correct: output == problem.answer
   end
 end
