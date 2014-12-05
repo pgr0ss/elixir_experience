1d06fcbf58926b2a02d8100dcb94fa77cc817475
bjhaid <abejide.ayodele@getbraintree.com>
2014-12-05 13:34:03 -0600

    Some refractor.

--- web/models/docker.ex
+++ web/models/docker.ex
@@ -1,21 +1,18 @@
 defmodule ElixirExperience.Docker do
   def run(code) do
     this = self
+    container_name = ElixirExperience.RandomStringGenerator.generate
     spawn(fn -> send(this, System.cmd("docker", ["run", "--net=none", "--name=\"#{container_name}\"", "trenpixster/elixir", "elixir", "-e", code], stderr_to_stdout: true)) end)
     {output, exit_code} = receive do
       any -> any
     after(2000) ->
       System.cmd("docker", ["kill", container_name])
-      System.cmd("docker", ["rm", container_name])
 
-      {"", 124}
+      {"Your code took longer than 2 seconds to run", 124}
     end
 
-    {String.strip(output), exit_code}
-  end
+    System.cmd("docker", ["rm", container_name])
 
-  defp container_name do
-    :random.seed(:os.timestamp)
-    Enum.map(1..20, fn _ -> (:random.uniform * 25 + 65) |> round end) |> to_string
+    {String.strip(output), exit_code}
   end
 end
