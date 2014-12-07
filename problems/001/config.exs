72e14bc09f79ff6f4afc5743ac549f2deeb2e028
bjhaid <abejideayodele@gmail.com>
2014-12-06 21:56:19 -0600

    Move problem definitions into an config files.

---
+++ problems/001/config.exs
@@ -0,0 +1,10 @@
+use Mix.Config
+
+config :"001",
+  problem: %ElixirExperience.Problem{
+    answer: "42",
+    question: """
+  Write an elixir program that prints the number 42 to stdout.
+    """,
+    number: 001
+  }
--- web/models/problem.ex
+++ web/models/problem.ex
@@ -10,16 +10,7 @@ defmodule ElixirExperience.Problem do
   end
 
   defp load_problem(directory) do
-    %ElixirExperience.Problem{
-      number: String.to_integer(directory),
-      question: read_file(directory, "question.txt"),
-      answer: read_file(directory, "answer.txt"),
-    }
-  end
-
-  defp read_file(directory, filename) do
-    Path.join([@problems_directory, directory, filename])
-    |> File.read!
-    |> String.strip
+    [{directory, [problem: config]}] = Path.join([@problems_directory, directory, "config.exs"]) |> Mix.Config.read!
+    config
   end
 end
