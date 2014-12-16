4c33e820896d194090c79492a8962563650ce053
bjhaid <abejideayodele@gmail.com>
2014-12-15 19:36:55 -0600

    Pull image check logic into a separate PR.

--- web/models/docker.ex
+++ web/models/docker.ex
@@ -4,16 +4,9 @@ defmodule ElixirExperience.Docker do
   @test_name "ElixirExperienceTest"
 
   def pull_image do
-    unless image_exists? do
-      require Logger
-      Logger.info "Pulling Docker image #{@image_name}"
-      {_output, 0} = System.cmd("docker", ["pull", @image_name], stderr_to_stdout: true)
-    end
-  end
-
-  defp image_exists? do
-    {output, 0} = System.cmd("docker", ["images"], stderr_to_stdout: true)
-    String.contains?(output, @image_name)
+    require Logger
+    Logger.info "Pulling Docker image #{@image_name}"
+    {_output, 0} = System.cmd("docker", ["pull", @image_name], stderr_to_stdout: true)
   end
 
   def run(code, problem, timeout \\ 5000) do
