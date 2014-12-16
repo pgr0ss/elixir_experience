a6783c3983a1c2ba1b6c1a3d86582fc723794a4c
bjhaid <abejideayodele@gmail.com>
2014-12-16 00:06:43 -0600

    clean up.

--- web/models/docker.ex
+++ web/models/docker.ex
@@ -12,10 +12,6 @@ defmodule ElixirExperience.Docker do
   defp image_exists? do
     {output, 0} = System.cmd("docker", ["images"], stderr_to_stdout: true)
     String.contains?(output, @image_name)
-
-    require Logger
-    Logger.info "Pulling Docker image #{@image_name}"
-    {_output, 0} = System.cmd("docker", ["pull", @image_name], stderr_to_stdout: true)
   end
 
   def run(code, timeout \\ 5000) do
