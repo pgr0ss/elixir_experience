bcb333456399f256a42ffdc6fb5b7e8b961e8dc1
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2015-05-23 02:08:10 -0300

    Add a better description for problem 5
    Related to #20

--- config/problems.exs
+++ config/problems.exs
@@ -81,10 +81,14 @@ config :problems,
   %Problem{
     number: 005,
     question: """
-    Write a function palindrome? that checks if a given string is a palindrome, A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.  e.g
+    Write a function palindrome? that checks if a given string is a palindrome. 
+    A palindrome is a word, phrase, number, or other sequence of characters which
+    reads the same backward or forward, ignoring non-word characters (like spaces, comma, exclamation marks, etc.)
+    e.g
 
     ```elixir
-      palindrome?("madam") #=> true
+      palindrome?("123,21") #=> true
+      palindrome?("Madam, I’m Adam.") #=> true
     ```
     """,
     solution: """
